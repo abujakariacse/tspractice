4a36d57f005c0227421fd973cce37658fce85df0
Abu Jakaria <abujakaria316@gmail.com>
2025-01-16 02:00:11 +0600

    git initialized

---
+++ src/basicDataTypes.ts
@@ -0,0 +1,27 @@
+// Basic data types
+
+// string
+let firstName: string = "Jakaria";
+
+// number
+let roll: number = 38;
+
+// boolean
+let isAdmin: boolean = true;
+
+// undefined
+let x: undefined = undefined;
+
+// null
+
+let y: null = null;
+
+// Array
+
+let friends: string[] = ["jack", "mack", "rack"];
+
+let rollList: number[] = [1, 3, 4, 5, 6];
+
+// touple -> maintain order of types
+
+let nameRollSmoker: [string, number, boolean] = ["Jakaria", 13, true];
---
+++ src/objectLiteralOptional.ts
@@ -0,0 +1,12 @@
+// Refference type --> Object
+
+const user: {
+  firstName: string;
+  middleName?: string; //optional type
+  lastName: string;
+  age: number;
+} = {
+  firstName: "Abu",
+  lastName: "Jakaria",
+  age: 23,
+};
