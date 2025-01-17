4a18d2c3634226b94719b74c9c0547b1f0d90366
Abu Jakaria <abujakaria316@gmail.com>
2025-01-17 20:14:05 +0600

    unknown, nullable

---
+++ src/neverUnknownNullable.ts
@@ -0,0 +1,27 @@
+{
+  // nullable type
+
+  const searchName = (value: string | null) => {
+    if (value) {
+      console.log("Searching");
+    } else {
+      console.log("There is nothing to search");
+    }
+  };
+
+  searchName(null);
+
+  //   unknown type
+  const getSpeedInMeterPerSecond = (value: unknown) => {
+    if (typeof value === "number") {
+      const converted = (value * 1000) / 3600;
+      console.log(`The speed is ${converted} ms^-1`);
+    } else if (typeof value === "string") {
+      const [result] = value.split(" ");
+      const convertedString = (parseInt(result) * 1000) / 3600;
+      console.log(`The speed is ${convertedString} ms^-1`);
+    } else {
+      console.log("Wrong input");
+    }
+  };
+}
