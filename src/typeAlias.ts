c4a266b72c5dff5eebecc8424b479a4fc75be46c
Abu Jakaria <abujakaria316@gmail.com>
2025-01-17 19:24:21 +0600

    type alias

--- src/typeAlias.ts
+++ src/typeAlias.ts
@@ -1,3 +1,24 @@
 {
   // Type alias
+
+  type TUser = {
+    name: string;
+    roll: number;
+    class: string;
+    company: "Programming Hero";
+  };
+
+  const user1: TUser = {
+    name: "Abu Jakaria",
+    roll: 457072,
+    class: "Eleven",
+    company: "Programming Hero",
+  };
+
+  const user2: TUser = {
+    name: "Jack",
+    roll: 32,
+    class: "Six",
+    company: "Programming Hero",
+  };
 }
