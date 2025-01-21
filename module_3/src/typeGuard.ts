df19a6745aacd4f01eb47aaf9d531dbdd7aa292d
Abu Jakaria <abujakaria316@gmail.com>
2025-01-21 14:02:18 +0600

    type guard in operator

--- module_3/src/typeGuard.ts
+++ module_3/src/typeGuard.ts
@@ -2,7 +2,6 @@
   //Type Guard/Type Narrowing
 
   //   Type of operator
-
   type AlphaNumeric = string | number;
   const add = (num1: AlphaNumeric, num2: AlphaNumeric): AlphaNumeric => {
     if (typeof num1 === "number" && typeof num2 === "number") {
@@ -14,4 +13,27 @@
   console.log(add(2, 4));
   console.log(add("2", "6"));
   console.log(add("2", 5));
+
+  //   In guard
+  type NormalUser = {
+    name: string;
+  };
+  type AdminUser = {
+    name: string;
+    role: "admin";
+  };
+
+  const getUser = (user: NormalUser | AdminUser) => {
+    if ("role" in user) {
+      console.log(`My name is ${user.name}. My role is ${user.role}`);
+    } else {
+      console.log(`My name is ${user.name}.`);
+    }
+  };
+
+  const normalUser: NormalUser = { name: "Mr. Normal" };
+  const adminUser: AdminUser = { name: "Admin User", role: "admin" };
+
+  getUser(normalUser);
+  getUser(adminUser);
 }
