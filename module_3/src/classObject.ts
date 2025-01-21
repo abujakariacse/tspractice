54c26730ae0bcf5105954f05b23eccd83b07dc65
Abu Jakaria <abujakaria316@gmail.com>
2025-01-21 13:06:52 +0600

    parameter properties

--- module_3/src/classObject.ts
+++ module_3/src/classObject.ts
@@ -1,19 +1,29 @@
 {
   // Class and Object
   class Animal {
-    name: string;
-    species: string;
-    sound: string;
+    // name: string;
+    // species: string;
+    // sound: string;
 
-    constructor(name: string, species: string, sound: string) {
-      this.name = name;
-      this.species = species;
-      this.sound = sound;
+    // Parameter properties
+    constructor(
+      public name: string,
+      public species: string,
+      public sound: string
+    ) {
+      //   this.name = name;
+      //   this.species = species;
+      //   this.sound = sound;
+    }
+    makeSound() {
+      console.log(`The ${this.name} sounds ${this.sound}`);
     }
   }
 
   const dog = new Animal("Tommy", "Dog", "ghew ghew");
+  dog.makeSound();
   const cat = new Animal("Persian", "Cat", "Meaw meaw");
+  cat.makeSound();
   console.log(dog);
   console.log(cat);
 }
