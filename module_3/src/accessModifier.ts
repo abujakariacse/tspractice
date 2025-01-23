5950387fab71363f638a1b8d0b3c08f13a3ba74e
Abu Jakaria <abujakaria316@gmail.com>
2025-01-24 00:54:49 +0600

    access modifier

---
+++ module_3/src/accessModifier.ts
@@ -0,0 +1,31 @@
+{
+  // Access modifiers
+  class BankAccount {
+    public readonly id: number;
+    public name: string;
+    protected _balance: number; //When we want the prop in child class and not in instance we will use protected
+
+    constructor(id: number, name: string, balance: number) {
+      this.id = id;
+      this.name = name;
+      this._balance = balance;
+    }
+    addDeposit(amount: number) {
+      this._balance = this._balance + amount;
+    }
+    getBalance() {
+      return this._balance;
+    }
+  }
+
+  class ProAccount extends BankAccount {
+    test() {
+      console.log(this._balance);
+    }
+  }
+
+  const goribManusherAccount = new BankAccount(1, "Mr. Gorib", 20);
+  goribManusherAccount.addDeposit(40);
+  const balance = goribManusherAccount.getBalance();
+  console.log(balance);
+}
